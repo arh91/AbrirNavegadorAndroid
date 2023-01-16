97b17e581f1a9ba4ae212e6d19ebd158122ffea0
arh91 <alvaro_rh_991@hotmail.com>
2023-01-16 16:05:16 +0100

    aplicación que abre el navegador

---
+++ app/src/main/java/com/example/navegador/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.navegador
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // El botón para iniciar el teléfono
+        val callButton = findViewById<Button>(R.id.call)
+        // numero de telefono al que queremos llamar
+        val url = findViewById<EditText>(R.id.url)
+
+
+        callButton.setOnClickListener{
+            // creamos un intent específico que iniciará el teléfono
+            val intent = Intent(Intent.ACTION_VIEW)
+            // rellenamos el intent con el número
+            intent.data = Uri.parse(url.text.toString())
+            // llamamos a la activity treléfono
+            startActivity(intent)
+        }
+    }
+}
